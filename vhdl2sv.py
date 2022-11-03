7004e9d2bd3ecc751c7e6ea3f74d7dda7a23ff71
Matthew-Otto <106996253+Matthew-Otto@users.noreply.github.com>
2022-11-03 11:56:25 -0500

    update some regex

--- vhdl2sv.py
+++ vhdl2sv.py
@@ -1,11 +1,15 @@
 # This will convert many common VHDL patterns to their SystemVerilog equivalents
+# All .vhd files in <dir> will be copied to respective .sv files with rough conversions
 
 import os
 import re
 
+dir = r"./VHDL/div_rtl"
+
 regex_list = [ # (re.compile(r"", re.IGNORECASE), ""),
     (re.compile(r"--", re.IGNORECASE), "//"),  # comments
     (re.compile(r"\(([0-9]+)\)", re.IGNORECASE), "[\g<1>]"),  # (2) to [2]
+    (re.compile(r"\(\s*([^\s]+)\s*downto\s*([^\s\)]+)\s*\)", re.IGNORECASE), "[\g<1>:\g<2>]"),
     (re.compile(r"([^\s]*\s+)<=", re.IGNORECASE), "assign \g<1>="),  # "var <= val" to "assign var = val"
 
     # Operators
@@ -15,36 +19,42 @@ regex_list = [ # (re.compile(r"", re.IGNORECASE), ""),
     (re.compile(r"([^\w])not([^\w])", re.IGNORECASE), "\g<1>~\g<2>"),
     
     # "var : in std_logic_vector(PARAM-1 downto 0);" to "input logic [PARAM-1:0] var,"
-    (re.compile(r"([^\s]+)\s+:\s+in\s+std_logic_vector\(([^\s]+)\s*downto\s*(\d)\);", re.IGNORECASE), "input  logic [\g<2>:\g<3>] \g<1>,"),
-    (re.compile(r"([^\s]+)\s+:\s+in\s+std_logic_vector\(([^\s]+)\s*downto\s*(\d)\)+;", re.IGNORECASE), "input  logic [\g<2>:\g<3>] \g<1>);"),
+    (re.compile(r"([^\s]+)\s+:\s+in\s+std_logic_vector\s*(\[[^\s]+:[^\s]+\])\s*;", re.IGNORECASE), "input  logic \g<2> \g<1>,"),
+    (re.compile(r"([^\s]+)\s+:\s+in\s+std_logic_vector\s*(\[[^\s]+:[^\s]+\])\s*(\);)", re.IGNORECASE), "input  logic \g<2> \g<1>);"),
     # "var : in std_logic;" to "input  logic var,"
     (re.compile(r"([^\s]+)\s+:\s+in\s+std_logic;", re.IGNORECASE), "input  logic \g<1>,"),
     (re.compile(r"([^\s]+)\s+:\s+in\s+std_logic\);", re.IGNORECASE), "input  logic \g<1>);"),
     
     # "var : out std_logic_vector(PARAM-1 downto 0);" to "output logic [PARAM-1:0] var,"
-    (re.compile(r"([^\s]+)\s+:\s+out\s+std_logic_vector\(([^\s]+)\s*downto\s*(\d)\);", re.IGNORECASE), "output logic [\g<2>:\g<3>] \g<1>,"),
-    (re.compile(r"([^\s]+)\s+:\s+out\s+std_logic_vector\(([^\s]+)\s*downto\s*(\d)\)+;", re.IGNORECASE), "output logic [\g<2>:\g<3>] \g<1>);"),
+    (re.compile(r"([^\s]+)\s+:\s+out\s+std_logic_vector\s*(\[[^\s]+:[^\s]+\])\s*;", re.IGNORECASE), "output logic \g<2> \g<1>,"),
+    (re.compile(r"([^\s]+)\s+:\s+out\s+std_logic_vector\s*(\[[^\s]+:[^\s]+\])\s*(\);)", re.IGNORECASE), "output logic \g<2> \g<1>);"),
     # "var : out std_logic;" to "output logic var,"
     (re.compile(r"([^\s]+)\s+:\s+out\s+std_logic;", re.IGNORECASE), "output logic \g<1>,"),
     (re.compile(r"([^\s]+)\s+:\s+out\s+std_logic\);", re.IGNORECASE), "output logic \g<1>);"),
     
     # "signal var : std_logic_vector(PARAM-1 downto 0)" to "logic [PARAM-1:0] var"
-    (re.compile(r"signal\s([^\s]+)\s+:\s+std_logic_vector\(([^\s]+)\s*downto\s*(\d).*;", re.IGNORECASE), "logic [\g<2>:\g<3>] \g<1>;"),
+    (re.compile(r"signal\s([^\s]+)\s+:\s+std_logic_vector\s*(\[[^\s]+:[^\s]+\]).*;", re.IGNORECASE), "logic \g<2> \g<1>;"),
     # "signal var : std_logic" to "logic var"
     (re.compile(r"signal\s([^\s]+)\s+:\s+std_logic", re.IGNORECASE), "logic \g<1>"),
+
+    # port assignments for module instantiation
+    (re.compile(r"([^\s]+)\s+=>\s+([^\s,\(\)]+)(,|\);)?", re.IGNORECASE), ".\g<1>(\g<2>)\g<3>"),
+    (re.compile(r"([^\s]+)\s+=>\s+([^\s,\(\)]+)\((\d+)\s+downto\s+(\d+)\)(,|\);)?", re.IGNORECASE), ".\g<1>(\g<2>[\g<3>:\g<4>])\g<5>"),
 ]
 
 
 def main():
-    dir = r"./VHDL/div_rtl"
+    svfilelist = []
     for filename in os.listdir(dir):
         if not filename.endswith(".vhd"):
             continue
-
+        
         # make a copy of .vhd file to .sv
         oldpath = os.path.join(dir, filename)
         new_filename = filename.replace(".vhd", ".sv")
         newpath = os.path.join(dir, new_filename)
+
+        svfilelist.append(new_filename)
         
         with open(oldpath, "r") as file:
             file_contents = file.readlines()
@@ -54,6 +64,7 @@ def main():
                 file_contents = [regex.sub(replace, line) for line in file_contents]
             file.writelines(file_contents)
 
+    print(" ".join(svfilelist), file=open('modules.txt', 'w'))
 
 
 if __name__ == "__main__":
